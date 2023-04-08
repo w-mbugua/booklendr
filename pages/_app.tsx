ca5f5ba823c22eecc2edfb79ff2716b6a13d66f1
JoyMbugua <mbuguajoy52@gmail.com>
2023-04-08 06:19:11 +0300

    project config setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import type { AppProps } from 'next/app';
+
+const client = new ApolloClient({
+  uri: process.env.NEXT_PUBLIC_API_URL,
+  cache: new InMemoryCache(),
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </ChakraProvider>
+  );
 }
