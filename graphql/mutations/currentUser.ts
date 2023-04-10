5034db708ae6fc90e74218c01de3ecf294ceb456
JoyMbugua <mbuguajoy52@gmail.com>
2023-04-10 20:09:47 +0300

    add auth queries

---
+++ graphql/mutations/currentUser.ts
@@ -0,0 +1,49 @@
+import { graphql } from "@/generated/gql";
+
+const currentUser = graphql(`
+query CurrentUser {
+  currentUser {
+    email
+    phoneNumber
+    username
+    books {
+      title
+      tags {
+        name
+      }
+      status
+      author {
+        name
+      }
+      loans {
+        borrower {
+          username
+          phoneNumber
+          email
+        }
+        createdAt
+        returnDate
+        book {
+          title
+          id
+        }
+      }
+      reservations {
+        reserver {
+          username
+          phoneNumber
+          email
+        }
+        status
+        createdAt
+        updatedAt
+        book {
+          title
+          id
+        }
+      }
+    }
+    id
+  }
+}
+`)
