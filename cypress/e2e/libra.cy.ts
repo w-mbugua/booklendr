a1d60945c6220382f8ecaba97bc23e19dc4d2eda
JoyMbugua <mbuguajoy52@gmail.com>
2023-04-28 06:11:09 +0300

    add initial tests

--- cypress/e2e/libra.cy.ts
+++ cypress/e2e/libra.cy.ts
@@ -1,19 +1,58 @@
 describe('Pagepals', () => {
-  beforeEach(function() {
-    cy.visit('http://localhost:3001/');
-  })
-
-  it('loads login', () => {
-    cy.contains('Welcome');
-    cy.contains('Enter Your Credentials To Login');
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:4000/testing/reset');
+    cy.visit('http://localhost:3000/register');
+    cy.get('#username').type('User One');
+    cy.get('#email').type('user1@gmail.com');
+    cy.get('#phoneNumber').type('0708200200');
+    cy.get('#password').type('pass123');
+    cy.get('#register-btn').click();
   });
 
-  it('login form can be clicked', function () {
-    cy.contains('Login').click()
+  describe('login', function () {
+    beforeEach(function () {
+      cy.visit('http://localhost:3000/');
+    });
+
+    it('loads login', function () {
+      cy.contains('Welcome');
+      cy.contains('Enter Your Credentials To Login');
+    });
+
+    it('login form can be clicked', function () {
+      cy.contains('Login').click();
+    });
+
+    it('user can login', function () {
+      cy.contains('Login').click();
+      cy.get('#username').type('user1@gmail.com');
+      cy.get('#password').type('pass123');
+      cy.get('#login-btn').click();
+
+      cy.contains('Welcome, User One');
+    });
   });
-  it('user can login', function() {
-    cy.contains('Login').click()
-    cy.get('input:first').type('jdoe@email.com')
-    cy.get('input:last').type('1234567')
-  })
+
+  describe('a book exists', function () {
+    beforeEach(function () {
+      cy.contains('New Book').click();
+    });
+
+    it('should display error if any field is empty', function () {
+      cy.get('#book-title').type('test book')
+      cy.get('#new-book-btn').click()
+
+      cy.get('#author-error').should('contain.text', 'Author is required')
+      cy.get('#tag-error').should('contain.text', 'Book category is required')
+    })
+
+    it('a new book can be added', function () {
+      cy.get('#book-title').type('the pragmatic programmer')
+      cy.get('#author').type('andrew hunt')
+      cy.get('#tag').type('software engineering')
+      cy.get('#new-book-btn').click()
+      cy.get('#book-post').contains('The Pragmatic Programmer');
+      cy.get('#book-post').contains('View More').click();
+    });
+  });
 });
