0cb28ce001a19091f44236aabfaeb6ea68969ec8
JoyMbugua <mbuguajoy52@gmail.com>
2023-05-05 07:34:39 +0300

    feat:search books

---
+++ graphql/queries/searchBooks.ts
@@ -0,0 +1,11 @@
+import { graphql } from '@/generated/gql';
+
+const SEARCH_BOOKS = graphql(`
+  query SEARCH_BOOKS($searchTerm: String!) {
+    searchBook(searchTerm: $searchTerm) {
+      id
+      title
+	  cover
+    }
+  }
+`);
